58d735cff7324456d60865b09c6353371726472c
Dan King <king.daniel.thomas@gmail.com>
2023-12-05 10:31:52 +0700

    Drill a function into cart for updating quantity

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -5,47 +5,47 @@ import {Link, Outlet} from "react-router-dom";
 
 export function Layout () {
   const [cartItems, setCartItems] = useState([])
+  const updateProductQuantity = (e, productId) => {
+    console.log(`updating quantity at ${e.target}`)
+    // if (e.target.textContent == '-'){
+    //   const productForAdjustment = cartItems.filter((item) => item.id == productId)
+    //   productForAdjustment.quantity -= 1
+    //   console.log(productForAdjustment)
+    // } else if (e.target.textContent == '+'){
+    //   const productForAdjustment = cartItems.filter((item) => item.id == productId)
+    //   productForAdjustment.quantity += 1 
+    //   console.log(productForAdjustment)
+    // }
+  }
 
   return (
     <>
-      <Navcontainer />
+      <Navcontainer onUpdate={updateProductQuantity} />
       <GhostNav />
       <Outlet context={[cartItems, setCartItems]}/>  
     </>
   )
 
 
-  function Navcontainer () {
+  function Navcontainer ({onUpdate}) {
 
     return (
       <>
-      <div className="
-        flex
-        justify-between
-        items-center
-        border
-        border-black
-        min-w-full
-        p-4
-        fixed
-        z-10
-        bg-white"
-      >
+      <div className="flex justify-between items-center border border-black min-w-full p-4 fixed z-10 bg-white">
       <nav>
-        <ul className="
-          flex
-          gap-8"
-        >
+        <ul className="flex gap-8">
           <li><Link to="/">Home</Link></li>
           <li><Link to="/shop">Shop Products</Link></li>
           <li><Link to="/pages/about">About Us</Link></li>
         </ul>
       </nav>
-      <Cart cartItems={cartItems} />
+      <Cart cartItems={cartItems} onUpdate={onUpdate} />
       </div>
       </>
     )
   }
+
+
 }
 
 function GhostNav () {
@@ -53,5 +53,7 @@ function GhostNav () {
     <div className="sticky-bumper"></div>
   )
 }
+
+
 
 export default Layout;
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import "../styles/cart.css"
 
-export default function Cart ({cartItems}) {
+export default function Cart ({cartItems, onUpdate}) {
 
   const [isOpen, setIsOpen] = useState(false)
   const [cartCount, setCartCount] = useState(0);
@@ -36,7 +36,7 @@ export default function Cart ({cartItems}) {
       <div className="cart-items clamp-width absolute top-20 right-0 z-20 bg-white border border-black p-4">
         <div className="text-xl"><h2>Your items:</h2></div>
         <hr></hr>
-        <CartItemsList cartItems={cartItems} />
+        <CartItemsList cartItems={cartItems} onUpdate={onUpdate} />
         <hr></hr>
         <button onClick={showCartObject} className="p-4 bg-blue-600 min-w-full">Check out</button>
       </div>
@@ -49,20 +49,7 @@ export default function Cart ({cartItems}) {
   )
 }
 
-const CartItemsList = ({cartItems}) => {
-
-  const inCartDecrement = (productId) => {
-    const productForAdjustment = cartItems.filter((item) => item.id == productId)
-    
-    productForAdjustment.quantity -= 1
-    console.log(productForAdjustment)
-  }
-
-  const inCartIncrement = (productId) => {
-    const productForAdjustment = cartItems.filter((item) => item.id == productId)
-    productForAdjustment.quantity += 1 
-    console.log(productForAdjustment)
-  }
+const CartItemsList = ({cartItems, onUpdate}) => {
   
   if (cartItems !== null){
     let subtotal = 0;
@@ -81,11 +68,11 @@ const CartItemsList = ({cartItems}) => {
           </div>
           <div className="right-side min-w-max p-2 flex items-center justify-center">
             <div className="in-cart-quantities">
-              <button onClick={() => inCartDecrement(item.id)} className="adjustment-btn decrement py-0 px-2">&minus;</button>
+              <button onClick={onUpdate} className="adjustment-btn decrement py-0 px-2">&minus;</button>
               <span className="relative mx-2 text-center bg-green-500 text-white rounded-[50%] inline-block h-[24px] w-[24px]">
                 {item.quantity}
               </span>
-              <button onClick={() => inCartIncrement(item.id)} className="adjustment-btn increment py-0 px-2">+</button>
+              <button onClick={() => onUpdate(item.id)} className="adjustment-btn increment py-0 px-2">+</button>
               <span className="text-red-500 p-2">Remove</span>
             </div>
           </div>
